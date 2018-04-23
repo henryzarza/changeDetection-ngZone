11ec08a35ade268cc4243e7328898640ca651130
Henry Zarza <henryacz7@gmail.com>
2018-04-23 10:09:36 -0500

    Advance in structure - ngZone

--- src/app/ng-zone/ng-zone.component.ts
+++ src/app/ng-zone/ng-zone.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-ng-zone',
   templateUrl: './ng-zone.component.html',
   styleUrls: ['./ng-zone.component.css']
 })
-export class NgZoneComponent implements OnInit {
+export class NgZoneComponent {
 
-  constructor() { }
+  progress: number = 0;
+  label: string;
+  time: number = 10;
 
-  ngOnInit() {
+  constructor(private zone: NgZone) { }
+
+  // Loop inside the Angular zone
+  // so the UI DOES refresh after each setTimeout cycle
+  processWithinAngularZone() {
+    this.label = 'inside';
+    this.progress = 0;
+    this._increaseProgress(() => console.log('Inside Done!'));
   }
 
+  // Loop outside of the Angular zone
+  // so the UI DOES NOT refresh after each setTimeout cycle
+  processOutsideOfAngularZone() {
+    this.label = 'outside';
+    this.progress = 0;
+    this.zone.runOutsideAngular(() => {
+      this._increaseProgress(() => {
+        // reenter the Angular zone and display done
+        this.zone.run(() => { console.log('Outside Done!'); });
+      });
+    });
+  }
+
+  _increaseProgress(doneCallback: () => void) {
+    this.progress += 1;
+    console.log(`Current progress: ${this.progress}%`);
+
+    if (this.progress < 100) {
+      window.setTimeout(() => this._increaseProgress(doneCallback), 10);
+    } else {
+      doneCallback();
+    }
+  }
 }
